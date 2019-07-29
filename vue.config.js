8f8e4034c41998df8c0f3792a3519cb943827ceb
Marcin Filipowski <proartz.devon@gmail.com>
2019-07-29 18:36:44 +0200

    Change host to 192.178.1.21

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        host: '192.168.1.21',
+        port: 8080,
+        // headers: {
+        //     "Access-Control-Allow-Origin": "*",
+        //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        //     "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        // }
+    }
+}
+  
